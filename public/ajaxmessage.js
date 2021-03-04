0136c099a92718456a3736126276c0f8d2c15595
Teemu Nasanen <teemu.nasanen@student.laurea.fi>
2021-03-04 04:55:52 +0200

    edit

--- public/ajaxmessage.js
+++ public/ajaxmessage.js
@@ -6,9 +6,14 @@ $(() => {
       country: $("#country").val(),
       message: $("#message").val(),
     };
+    if ($("#username").val() == "" ||$("#country").val() == ""||$("#message").val() == ""){
+      alert("Every field is important!");
+    }else{
    
     $.post("/ajaxmessage", data, function (res, status) {
       $("#status").html(res);
-    }); 
+    
+    });
+  } 
   }); 
 });
